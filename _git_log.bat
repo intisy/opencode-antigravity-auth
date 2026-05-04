8ef9567cc70b44255419d4dd52f115b6e457cb00
Intisy <finn@birich.de>
2026-05-04 21:18:31 +0200

    chore: clean up temporary script files

--- _git_log.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-cd /d "C:\Users\finn\.config\opencode\repos\opencode-antigravity-auth"
-git log -1 --stat
--- _git_push.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-set GIT_TERMINAL_PROMPT=0
-set GCM_INTERACTIVE=never
-set GIT_EDITOR=:
-set GIT_PAGER=cat
-set PAGER=cat
-cd /d "C:\Users\finn\.config\opencode\repos\opencode-antigravity-auth"
-git push origin main
-echo PUSH DONE
